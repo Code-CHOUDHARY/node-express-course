8c51246cad0f6503984a7f56dd3006ebc7007390
Devkaran <devkaransingh928@gamil.com>
2020-12-24 20:12:39 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const mockUserData=[
               users: mockUserData
          })
     })
-    
+    app.get('/users/:id',function(req,res){
+        console.log(req.params.id)
+        res.json({
+            success: true,
+            message: 'got one user',
+            user: req.params.id
+        })
+    })
 app.listen(8000,function(){
     console.log("server is running")
     })
